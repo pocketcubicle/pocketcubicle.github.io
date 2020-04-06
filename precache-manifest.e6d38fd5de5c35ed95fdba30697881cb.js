86295bbe061acbf53d9d1111928454c86f04afea
pocketcubicle <contact@pocketcubicle.com>
2020-04-06 13:59:12 +0400

    first commit

---
+++ precache-manifest.e6d38fd5de5c35ed95fdba30697881cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c06e458f9b060875efbf493451307af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2d0273094cf00b2318e",
+    "url": "/static/css/2.bf0413c3.chunk.css"
+  },
+  {
+    "revision": "5789fc5beddad031e1fe",
+    "url": "/static/css/main.77356c73.chunk.css"
+  },
+  {
+    "revision": "b2d0273094cf00b2318e",
+    "url": "/static/js/2.947680f9.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.947680f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5789fc5beddad031e1fe",
+    "url": "/static/js/main.5df66002.chunk.js"
+  },
+  {
+    "revision": "12678ada89c091429530",
+    "url": "/static/js/runtime-main.8cd2bf54.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
